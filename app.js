cf8ae4850ac309d05c5b1a80908be00d30444cc0
Aaron <astrauli@gmail.com>
2017-12-12 21:09:42 -0800

    add filters

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 import express from 'express';
 import mongodb, { MongoClient } from 'mongodb';
-import mongoose, { Schema } from 'mongoose';
+import mongoose, { Schema, Aggregate } from 'mongoose';
 import bodyParser from 'body-parser';
 
 mongoose.Promise = global.Promise;
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://root:bestappever@ds133856.mlab.com:33856/coffeewifi' ||'mongodb://localhost:27017/test'
+// process.env.MONGODB_URI ||
+// 'mongodb://root:bestappever@ds133856.mlab.com:33856/coffeewifi' ||
+mongoose.connect('mongodb://localhost:27017/test'
 , {useMongoClient: true});
 
+// let userSchema = new Schema({
+//   sub: String,
+//   liked_workspaces: {type: [Schema.Type.ObjectId]}
+// });
+
 let userSchema = new Schema({
-  sub: String
+  name: String,
+  title: String,
+  num: Number
 });
+
 
 let businessSchema = new Schema({
   name: String,
@@ -24,11 +33,14 @@ let Business = mongoose.model('businesses', businessSchema);
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
-
   let server = app.listen(process.env.PORT || 8080, () => {
     let port = server.address().port;
     console.log("App now running on port", port);
   });
+
+  // User.create({name: "Jeff",
+  //              title: "AA",
+  //              num: 233})
 });
 
 
@@ -69,6 +81,53 @@ app.post("/users", (req, res) => {
       res.json(user);
     }
   });
+});
+
+//test
+let filter;
+let name;
+let radius;
+let location;
+let outlets;
+let query;
+let aggregate;
+let pipeline;
+app.post('/filter', (req,res) => {
+   filter = req.body;
+  //  console.log(filter);
+   name = filter.name;
+   console.log(name);
+   radius = filter.radius;
+   location = filter.location;
+   outlets = filter.outlets;
+   console.log("r", radius);
+   console.log("l", location);
+   console.log("o", outlets);
+   aggregate = Business.aggregate()
+   if (name) {
+     aggregate = aggregate.match({name});
+   }
+   if (radius && location) {
+     aggregate = aggregate.near({
+           "near": location,
+           "distanceField": "dist.calculated",
+           "spherical": true,
+           "maxDistance": radius
+     })
+    //  console.log(aggregate.pipeline());
+   }
+   if (outlets) {
+     aggregate = aggregate.match({outlets})
+   }
+
+   aggregate.exec((err, result) => {
+     if (err) {
+       console.log(err);
+       res.json(err);
+     }
+     console.log(result);
+     res.json(result);
+   })
 });
 
 app.get('/businesses', (req, res) => {
